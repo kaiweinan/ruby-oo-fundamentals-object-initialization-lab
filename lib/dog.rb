9de89561789b5e4a2161117d39a83127de59ceaf
kaiweinan <kaiweinan@yahoo.com>
2021-01-03 13:45:52 -0500

    commit tree 0f83a70bab791cd5b2405268039741660455570e

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Person
+    def initialize(name)
+        @name=name
+    end
+    def name
+        @name=name
+    end
+end
+
+class Dog
+    def initialize(name, breed="Mutt")
+        @name = name
+        @breed=breed
+
+    end
+
+    def name
+        @name=name
+    end
+    def breed
+        @breed=breed
+    end
+end
